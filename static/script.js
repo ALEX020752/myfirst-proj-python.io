a09f410769c39140dd01d1d74b477a390613c671
ALEX020752 <159694757+ALEX020752@users.noreply.github.com>
2024-02-11 17:30:21 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('myVideo');
+    var playButton = document.getElementById('playButton');
+
+    playButton.addEventListener('click', function() {
+        if (video.paused) {
+            video.play();
+            playButton.textContent = 'Пауза';
+        } else {
+            video.pause();
+            playButton.textContent = 'Воспроизвести видео';
+        }
+    });
+});
+
